3dcc3cfd9ef4a31a402cbf8e498343282883ba32
nithi <nithi@Nithish>
2026-08-01 15:04:13 +0530

    First commit

--- src/test/java/SwagDemo/demo.java
+++ src/test/java/SwagDemo/demo.java
@@ -3,7 +3,8 @@ package SwagDemo;
 public class demo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("This is my first commit");
 
 	}
 
