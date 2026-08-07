ce5b69a574938fce503c51a264c2a251cce7ef48
nithi <nithi@Nithish>
2026-08-07 18:26:32 +0530

    sample111 is commited

--- src/test/java/SwagDemo/sample111.java
+++ src/test/java/SwagDemo/sample111.java
@@ -1,13 +1,10 @@
 package SwagDemo;
 
-public class sample3 {
+public class sample111 {
 
 	public static void main(String[] args) {
-		int a=10;
-		int b=19;
-		int c=a+b;
-		System.out.println(c);
 		System.out.println("hello");
+
 	}
 
 }
